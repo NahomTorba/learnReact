4d9cb3348267d5be30c4e17570c5c7e4209d6b2c
NahomTorba <nahumtorba@gmail.com>
2026-02-14 20:39:55 +0300

    fix condition on favorites

--- frontendMovies/src/pages/Favorites.jsx
+++ frontendMovies/src/pages/Favorites.jsx
@@ -6,22 +6,24 @@ function Favorites(){
 
     const {favorites} = useMovieContext();
     
-    if (favorites){
         return <div className="favorites">
+        {favorites.length > 0 ? (
+          <>
             <h2>Your Favorites</h2>
-        
-        <div className="movies-grid">
-        {favorites.map((movie) => (
-            <MovieCard movie={movie} key={movie.id} />
-        ))}
-        </div>
-    </div>
-    }else {
-        return <div className="favorites-empty"> 
+            <div className="movies-grid">
+              {favorites.map((movie) => (
+                <MovieCard movie={movie} key={movie.id} />
+              ))}
+            </div>
+          </>
+        ) : (
+          <div className="favorites-empty">
             <h2>No Favorites Yet</h2>
-            <p>start adding movies to your favorite</p>
-        </div>
-    }
+            <p>Start adding movies to your favorites</p>
+          </div>
+        )}
+      </div>
+    
     }
 
 export default Favorites;
